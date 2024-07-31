6f6f3fa351001398226aae4a79fa6f87f3d88d57
Adriano <adrianoalasia@gmail.com>
2024-07-31 15:36:01 -0400

    Icons

--- src/SimpleCollection/components/Header/index.js
+++ src/SimpleCollection/components/Header/index.js
@@ -11,7 +11,6 @@ export default (props) => {
 
   return (
     <div
-
       className={clsx(
         className,
         'fixed inset-x-0 top-0 z-50 flex h-14 items-center justify-between gap-12 px-4 transition sm:px-6 lg:left-72 lg:z-30 lg:px-8 xl:left-80',
@@ -22,13 +21,10 @@ export default (props) => {
           : 'bg-white/[var(--bg-opacity-light)] dark:bg-zinc-900/[var(--bg-opacity-dark)]',
       )}
 
-      style={
-        {
-          '--bg-opacity-light': '0.5',
-          '--bg-opacity-dark': '0.5',
-        }
-      }
-      
+      style={{
+          '--bg-opacity-light': '0.9',
+          '--bg-opacity-dark': '0.9',
+        }}      
     >
       <div
         className={clsx(
@@ -55,10 +51,11 @@ export default (props) => {
           </ul>
         </nav>
         <div className="hidden md:block md:h-5 md:w-px md:bg-zinc-900/10 md:dark:bg-white/15" />
-        {/* <div className="flex gap-4">
-          <MobileSearch />
+        
+        <div className="flex gap-4">
+          {/* <MobileSearch /> */}
           <ThemeToggle />
-        </div> */}
+        </div>
 
       </div>
     </div>
@@ -81,5 +78,52 @@ const Search = () => {
       </button>
 
     </div>
+  )
+}
+
+
+
+
+import { useEffect, useState } from 'react'
+
+function SunIcon(props) {
+  return (
+    <svg viewBox="0 0 20 20" fill="none" aria-hidden="true" {...props}>
+      <path d="M12.5 10a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z" />
+      <path
+        strokeLinecap="round"
+        d="M10 5.5v-1M13.182 6.818l.707-.707M14.5 10h1M13.182 13.182l.707.707M10 15.5v-1M6.11 13.889l.708-.707M4.5 10h1M6.11 6.111l.708.707"
+      />
+    </svg>
+  )
+}
+
+function MoonIcon(props) {
+  return (
+    <svg viewBox="0 0 20 20" fill="none" aria-hidden="true" {...props}>
+      <path d="M15.224 11.724a5.5 5.5 0 0 1-6.949-6.949 5.5 5.5 0 1 0 6.949 6.949Z" />
+    </svg>
+  )
+}
+
+export function ThemeToggle() {
+  let { resolvedTheme, setTheme } = 'light' 
+  let otherTheme = resolvedTheme === 'dark' ? 'light' : 'dark'
+  let [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  return (
+    <button
+      type="button"
+      className="flex items-center justify-center w-6 h-6 transition rounded-md hover:bg-zinc-900/5 dark:hover:bg-white/5"
+      aria-label={mounted ? `Switch to ${otherTheme} theme` : 'Toggle theme'}
+      onClick={() => setTheme(otherTheme)}
+    >
+      <SunIcon className="w-5 h-5 stroke-zinc-900 dark:hidden" />
+      <MoonIcon className="hidden w-5 h-5 stroke-white dark:block" />
+    </button>
   )
 }
