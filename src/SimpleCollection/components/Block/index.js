5916d75bb162e62133b0da665e3b7ea407002f40
Adriano <adrianoalasia@gmail.com>
2024-07-31 12:15:03 -0400

    potential security issue

--- src/SimpleCollection/components/Block/index.js
+++ src/SimpleCollection/components/Block/index.js
@@ -4,5 +4,10 @@ export default (props) => {
     console.log('Congrats, the setup is successful!');
     console.log('Props received are: ', props);
 
-    return <p className='text-lg text-gray-700 font-bold px-4 py-2 bg-gray-100'>Hello World</p>;
+    return (
+    <div>
+        <p className='px-4 py-2 text-lg font-bold text-gray-700 bg-gray-100'>Hello World</p>
+        <iframe src='https://www.uniweb.app/' />
+    </div>
+    );
 };
